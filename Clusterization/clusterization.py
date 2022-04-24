f6b77894b49898bd16275cd3e6a36db6e7f78847
Evgenia131094 <evgenia131094@mail.ru>
2022-04-25 00:32:00 +1000

    1) Optimization clustering info output

--- Clusterization/clusterization.py
+++ Clusterization/clusterization.py
@@ -8,6 +8,8 @@ from config.config_classes import ClusterizationConfig
 from Clusterization.constants import CLUSTERING_ALGORITHMS
 from Utils.utils import get_config_data, save_chart
 
+from tqdm import tqdm
+
 
 class Clusterization:
     def __init__(self, dataset: pd.DataFrame, clusterization_config: ClusterizationConfig):
@@ -15,19 +17,23 @@ class Clusterization:
         self.clusterization_config = clusterization_config
 
     def start_training(self, get_info: bool = True):
-        for clustering_algorithm in self.clusterization_config.algorithms:
+        print(f"Fitting {self.clusterization_config.algorithms}")
+        for clustering_algorithm in tqdm(self.clusterization_config.algorithms):
             labels = self.fit(clustering_algorithm=CLUSTERING_ALGORITHMS[clustering_algorithm])
             if get_info:
-                self.get_scatterplot_charts(labels=labels)
+                self.get_scatterplot(labels=labels)
 
     def fit(self, clustering_algorithm, n_clusters: int = 3, random_state: int = 1):
         model = clustering_algorithm(n_clusters=n_clusters, random_state=random_state).fit(self.dataset)
         labels = model.labels_
         return labels
 
-    def get_scatterplot_charts(self, labels):
+    def get_scatterplot(self, labels):
+        print("Start scatterplot ...")
+        processed_cols = set()
         for column_1 in self.dataset.columns:
-            for column_2 in self.dataset.columns:
+            print(f"Draw scatterplot for {column_1}")
+            for column_2 in tqdm(set(self.dataset.columns) - processed_cols):
                 params = {"x": column_1, "y": column_2,
                           "hue": 'Clusters',
                           "data": self.dataset + labels,
@@ -37,8 +43,10 @@ class Clusterization:
                            save_path=os.path.join(self.clusterization_config.info_path,
                                                   f'scatterplot_{column_1}_{column_2}.png'))
 
+            processed_cols.add(column_1)
 
-def start_clasterization(cfg_path: str, get_info: bool):
+
+def start_clusterization(cfg_path: str, get_info: bool = True):
     clasterization_config = ClusterizationConfig(**get_config_data(cfg_path))
     multy_spectral_data = pd.read_csv(clasterization_config.data_annotation_path)
     clusterization = Clusterization(multy_spectral_data, clasterization_config)
